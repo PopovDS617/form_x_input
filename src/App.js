634e0413ac00b34a8838fc562141595b000072e7
PopovDS617 <popovds.bm@gmail.com>
2022-07-17 23:10:06 +0300

    form completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import SimpleInput from './components/SimpleInput';
+import React, { useState } from "react";
+import SimpleInput from "./components/SimpleInput";
 
-function App() {
+const App = () => {
+  // const [title, setTitle] = useState("");
+
+  // const SubmitHandler = (data) => {
+  //   console.log(data);
+  //   setTitle(data);
+  // };
+
   return (
     <div className="app">
       <SimpleInput />
     </div>
   );
-}
+};
 
 export default App;
--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,10 +1,53 @@
+import React, { useState } from "react";
+
 const SimpleInput = (props) => {
+  const [enteredName, setEnteredName] = useState("");
+
+  const [enteredNameIsTouched, setEnteredNameIsTouched] = useState(false);
+
+  const enteredNameIsValid = enteredName.trim() !== "";
+
+  const nameInputIsInvlid = !enteredNameIsValid && enteredNameIsTouched;
+
+  const nameInputChangeHandler = (event) => {
+    setEnteredName(event.target.value);
+  };
+
+  const nameInputBlurHandler = (event) => {
+    setEnteredNameIsTouched(true);
+  };
+
+  const formSubmissionHandler = (event) => {
+    event.preventDefault();
+
+    setEnteredNameIsTouched(true);
+    if (!enteredNameIsValid) {
+      return;
+    }
+
+    setEnteredName("");
+    setEnteredNameIsTouched(false);
+  };
+
+  const inputClasses = nameInputIsInvlid
+    ? "form-control invalid"
+    : "form-control ";
+
   return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+    <form onSubmit={formSubmissionHandler}>
+      <div className={inputClasses}>
+        <label htmlFor="name">{enteredName}</label>
+        <input
+          type="text"
+          id="name"
+          onChange={nameInputChangeHandler}
+          onBlur={nameInputBlurHandler}
+          value={enteredName}
+        />
       </div>
+      {nameInputIsInvlid && (
+        <p className="error-text">Name must not be empty</p>
+      )}
       <div className="form-actions">
         <button>Submit</button>
       </div>
